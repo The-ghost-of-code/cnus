cd114c79245ae43b054c39b92bb039299158dc39
vingking <taducvinh1812@gmail.com>
2026-01-08 10:39:01 +0700

    Update printk.h
    complete!

--- printk.h
+++ printk.h
@@ -24,4 +24,7 @@ void printk(Autodefine1 text1,const  Autodefine2* text2, const char* h = nullptr
     if (h && std::strcmp(h, ";") == 0) {
         std::cout << text1 << "; " << text2 << std::endl;
     }
+    if (h == nullptr) {
+        std::cout << text1 << text2 << std::endl;
+    }
 }
